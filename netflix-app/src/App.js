6c111cdc15d9e253b3dc07128deedb448c8b7778
Twan Box <twanbox@gmail.com>
2020-09-08 20:01:00 +0200

    fetching movies from tmdd - started mapping over the results to create row for each genre

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Row from './Row';
+import requests from './requests'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
